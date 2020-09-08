0c1df01006c7d0beafb72c501ca2b0efa358c3f4
Sheldon Frith <frith1993@yahoo.ca>
2020-09-08 15:13:05 -0400

    added build directory

---
+++ Simple-Mobile-Calculator/web-build/precache-manifest.fd25f443a1eadffe4029e4aca53c02f3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bd2e8246f5555782e99",
+    "url": "/static/js/app.1babc6d1.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "1c77095528dedecc37a1",
+    "url": "/static/js/2.c741aa62.chunk.js"
+  },
+  {
+    "revision": "242fabc1f692565e8ae54e4f9d047170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.c741aa62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "b27287954a8393ef9c70073b2fb0b988",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "09916a55511147614cd2d8b0eedca566",
+    "url": "/manifest.json"
+  }
+];
